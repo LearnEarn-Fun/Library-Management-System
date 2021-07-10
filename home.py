eea431963f8d5de85ab4826707dc7d75c2ed9711
Hridaya Agrawal <hridayahoney@gmail.com>
2021-07-10 15:33:13 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,6 @@ from utils import handle_page
 from models import Book, User
 from data_manager import get_data
 from requests import get
-from app_config import SQLALCHEMY_DATABASE_URI
 
 home = Blueprint('home', __name__)
 
@@ -28,4 +27,4 @@ def homepage():
             {"id": book.id, "name": book.name, "price": book.price.strip('\r\n'), "author": book.author, "image": book.img,
              "free": book.is_free, "request_details": book.request_details, "year": book.year,
              "taken_by": taken_name, "category": book.category})
-    return handle_page('index.html', results=results, database_uri=SQLALCHEMY_DATABASE_URI)
+    return handle_page('index.html', results=results)
