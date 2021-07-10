4c68cbb66203acba771f95d4755823bba5ada157
Hridaya Agrawal <hridayahoney@gmail.com>
2021-07-10 15:29:22 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,14 +3,16 @@ from utils import handle_page
 from models import Book, User
 from data_manager import get_data
 from requests import get
+from app_config import SQLALCHEMY_DATABASE_URI
 
 home = Blueprint('home', __name__)
 
 
 @home.route('/', methods=['GET', 'POST'])
 def homepage():
-    response = get(get_data()['website_config']['icon'])
-    with open("static/icon.png", "wb") as icon:
+    icon_url = get_data()['website_config']['icon']
+    response = get(icon_url)
+    with open(f"static/icon.{icon_url.split('.')[1]}", "wb") as icon:
         icon.write(response.content)
     results = []
     books = Book.query.order_by(Book.id.desc()).all()
@@ -26,4 +28,4 @@ def homepage():
             {"id": book.id, "name": book.name, "price": book.price.strip('\r\n'), "author": book.author, "image": book.img,
              "free": book.is_free, "request_details": book.request_details, "year": book.year,
              "taken_by": taken_name, "category": book.category})
-    return handle_page(endpoint='index.html', results=results)
+    return handle_page('index.html', results=results, database_uri=SQLALCHEMY_DATABASE_URI)
