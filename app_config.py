7dd112875e0562fa7168c14422a52a49a529a6cd
Hridaya Agrawal <hridayahoney@gmail.com>
2021-07-10 14:47:18 +0530

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,18 @@
+import os
+from werkzeug.security import generate_password_hash
+
+SECRET_KEY = os.environ.get('SECRET_KEY', generate_password_hash('verySpecialKeyToKeep'))
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USERNAME = os.environ.get('EMAIL', 'hridayahoney@gmail.com')
+MAIL_PASSWORD = secrets.PASSWORD
+MAIL_USE_TLS = True
+JSON_SORT_KEYS = False
+GOOGLE_CLIENT_ID = secrets.GOOGLE_CLIENT_ID
+GOOGLE_CLIENT_SECRET = secrets.GOOGLE_CLIENT_SECRET
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
